9037064040ff26ab1f67ea8eddbd37f37d25f781
gracemcken <grace.ellen.mckenna@gmail.com>
2022-09-08 21:33:24 +0100

    Add code to calculate two random num for game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
 function runGame() {
 
 }
@@ -22,10 +38,10 @@ function displayAdditionQuestion() {
 
 }
 
-function displaySubractQuestion() {
+function displaySubtractQuestion() {
 
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
